2c87868d7fd8de8d62ca9e89147dd782c3d9ad84
MichaelElmore1 <135364586+MichaelElmore1@users.noreply.github.com>
2023-09-29 09:59:38 -0500

    Update license

--- sensorhub-core/src/main/java/org/sensorhub/impl/comm/TCPCommProvider.java
+++ sensorhub-core/src/main/java/org/sensorhub/impl/comm/TCPCommProvider.java
@@ -8,7 +8,7 @@
  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  for the specific language governing rights and limitations under the License.
 
- Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
+ Copyright (C) 2012-2023 Sensia Software LLC. and Botts Innovative Research, Inc. All Rights Reserved.
  ******************************* END LICENSE BLOCK ***************************/
 
 package org.sensorhub.impl.comm;
