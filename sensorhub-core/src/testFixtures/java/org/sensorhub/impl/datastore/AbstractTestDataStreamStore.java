8ac671e58a9a04620ea33aadd7e6fec073bfecb3
Alex Almanza <115671044+earocorn@users.noreply.github.com>
2025-09-22 14:40:12 -0500

    [DB] Update obs database test to use correct system UID (#314)
    * Update obs database test to use correct system uid
    
    * Swap outputName and description

--- sensorhub-core/src/testFixtures/java/org/sensorhub/impl/datastore/AbstractTestDataStreamStore.java
+++ sensorhub-core/src/testFixtures/java/org/sensorhub/impl/datastore/AbstractTestDataStreamStore.java
@@ -149,17 +149,25 @@ public abstract class AbstractTestDataStreamStore<StoreType extends IDataStreamS
     {
         return addSimpleDataStreamWithDefinition(new FeatureId(sysID, PROC_UID_PREFIX+sysID.getIdAsLong()), outputName, "datastream description", validTime, definition);
     }
-    
-    
+
     protected DataStreamKey addSimpleDataStream(BigId sysID, String outputName, TimeExtent validTime) throws DataStoreException
     {
-        return addSimpleDataStream(new FeatureId(sysID, PROC_UID_PREFIX+sysID.getIdAsLong()), outputName, "datastream description", validTime);
+        return addSimpleDataStream(PROC_UID_PREFIX+sysID, sysID, outputName, validTime);
     }
-    
-    
+
+    protected DataStreamKey addSimpleDataStream(String uniqueID, BigId sysID, String outputName, TimeExtent validTime) throws DataStoreException
+    {
+        return addSimpleDataStream(new FeatureId(sysID, uniqueID), outputName, "datastream description", validTime);
+    }
+
     protected DataStreamKey addSimpleDataStream(BigId sysID, String outputName, String description, TimeExtent validTime) throws DataStoreException
     {
-        return addSimpleDataStream(new FeatureId(sysID, PROC_UID_PREFIX+sysID.getIdAsLong()), outputName, description, validTime);
+        return addSimpleDataStream(PROC_UID_PREFIX+sysID, sysID, outputName, description, validTime);
+    }
+
+    protected DataStreamKey addSimpleDataStream(String uniqueID, BigId sysID, String outputName, String description, TimeExtent validTime) throws DataStoreException
+    {
+        return addSimpleDataStream(new FeatureId(sysID, uniqueID), outputName, description, validTime);
     }
 
 
@@ -653,7 +661,7 @@ public abstract class AbstractTestDataStreamStore<StoreType extends IDataStreamS
     {
         addToExpectedResults(0, 2);
     }
-    
+
     
     protected void testAddAndSelectByOutputName_ExpectedResults()
     {
